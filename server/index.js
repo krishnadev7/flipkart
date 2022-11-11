d8261fb03b84054c6a72256a9367e088896227d6
krishnadev7 <krishnadevv12@gmail.com>
2022-11-11 22:04:50 +0530

    4th commit: server file added & connected to mongodb

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = 8000 || process.env.PORT
+
+
+const Connect = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('database connected to mongodb');
+    } catch (error) {
+        console.log('mongodb error',error.message);
+    }
+}
+
+app.listen(PORT,()=>{
+    Connect();
+    console.log(`server connected on http://localhost:${PORT}`);
+})
+
